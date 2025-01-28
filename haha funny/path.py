640ecf041490cb58795f9d6dcc53a3f0fd4926a1
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2025-01-28 14:50:38 -0600

    used for a different purpose now

--- haha funny/path.py
+++ haha funny/path.py
@@ -1,22 +1,18 @@
-import os
+import tkinter as tk
+from tkinter import filedialog
 
-video_prefixes = ['mp4', 'wav']
+def select_folder():
+    folder_path = filedialog.askdirectory()
+    
+    if folder_path:
+        print(f"Selected folder: {folder_path}")
+        return folder_path
+    else:
+        print("No folder selected")
 
-def load_videos():
-    for index in range(len(os.listdir("files"))):
-        prefix = os.path.abspath(os.path.curdir)
-        cfile = os.listdir(prefix + "\\files\\")[index]  
-        info_path = os.path.abspath('/'.join((prefix + "\\files\\" + cfile + "\\info.txt").split('\\')))
-        videos: dict[str, str | None] = {'foler_path': None, 'video_path': None}
-        with open(info_path, "r") as info:
-            for line in info:
-                variable, value = line.split('=')
-                variable, value = (variable.strip(), value.strip())
-                videos[variable] = value
-            videos['folder_path'] = info_path[:-8]
-            info.close()
-        for nested_file in os.listdir(f"files/{cfile}"):
-            if nested_file[-3:] in video_prefixes:
-                videos['video_path'] = os.path.abspath(nested_file)
-    return videos
-load_videos()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    select_folder()
+    root.quit()
+
