81d201e3b54be074f160fb11559229f222416376
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2025-02-11 15:03:04 -0600

    A bit better error handling + update url soon

--- haha funny/api.py
+++ haha funny/api.py
@@ -3,22 +3,22 @@ import pygame
 import base64
 import os
 
-url = 'http://192.168.1.14:50000'
-
+url = 'https://onrender.pytorrent.com'
 
 def get_posts():
     try:
         response = requests.get(url)
 
-        if response.status_code == 200:
-            posts = response.json()
-
-            for post in posts:
-                post.update({"rect": pygame.Rect(0, 0, 200, 200)})
-            return posts
-        else:
-            print('Error:', response.status_code)
+        if response.status_code != 200:
+            print(f"Yikes got error code '{response.status_code}' you should probably report this to the devs")
             return None
+
+        posts = response.json()
+
+        for post in posts:
+            post.update({"rect": pygame.Rect(0, 0, 200, 200)})
+        return posts
+    
     except requests.exceptions.RequestException as e:
         print('Error:', e)
         return None
@@ -27,15 +27,19 @@ def download_directory(parent_folder_name):
     os.mkdir(f"files/{parent_folder_name}")
     folder_path = os.path.join(os.getcwd(), 'files', parent_folder_name)
     series = folder_path.split('\\')[-1]
+    
     try:
         responses = requests.get(url+f'/get/{series}')
-        if responses.status_code == 200:
-            for response in responses.json():
-                with open(folder_path+'/video.mp4', 'wb') as videoFile:
-                    videoFile.write(base64.b64decode(response['binary']))
-                
-                with open(folder_path+'/info.txt', 'w') as infoFile:
-                    infoFile.write(f'title = "{response['title']}"\ndate_released = "{response['year released']}"\nstudio = "{response['studio']}"\nrating = "{response['rating']}"')
+        if responses.status_code != 200:
+            print(f"Yikes got error code '{responses.status_code}' you should probably report this to the devs")
+            return None
+
+        for response in responses.json():
+            with open(folder_path+'/video.mp4', 'wb') as videoFile:
+                videoFile.write(base64.b64decode(response['binary']))
+            
+            with open(folder_path+'/info.txt', 'w') as infoFile:
+                infoFile.write(f'title = "{response["title"]}"\ndate_released = "{response["year released"]}"\nstudio = "{response["studio"]}"\nrating = "{response["rating"]}"')
 
     except requests.exceptions.RequestException as e:
         print('Error:', e)
