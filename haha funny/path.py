26b42fcb2cb121a5a8db95da6192fcac52aa7f23
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2025-02-14 15:04:20 -0600

    Update path.py

--- haha funny/path.py
+++ haha funny/path.py
@@ -3,18 +3,18 @@ from tkinter import filedialog
 import os
 
 def select_folder():
-    folder_path = filedialog.askdirectory()
+    return os.path.abspath(os.curdir)   # Auto path
+    
+    folder_path = filedialog.askdirectory()    # Manual Path
     
     if folder_path:
         print(f"Selected folder: {folder_path}")
         return folder_path
     else:
         print("No folder selected")
-    # return os.path.abspath(os.curdir) # Potential optimization :)
-
+        
 if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()
     select_folder()
     root.quit()
-
