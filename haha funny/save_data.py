fb477808ac0680c8fd1415985b025bc48a56fc78
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2025-03-21 14:58:32 -0500

    lil bit more efficient

--- haha funny/save_data.py
+++ haha funny/save_data.py
@@ -10,13 +10,9 @@ def get_main_directory():
     try:
         import temp_data
         return temp_data.Path 
-    except AttributeError:
-        # Attribute doesn't exist yet
-        folder_path = path.select_folder()
-        save({'Path': folder_path})
-        return folder_path
-    except ModuleNotFoundError:
-        with open("temp_data.py", 'w') as file:  # noqa: F841   # File doesn't exist yet 
-            pass
+    except Exception:
+        with open("temp_data.py", 'w') as file:  # noqa: F841  # File doesn't exist yet 
+            folder_path = path.select_folder()
+            save({'Path': folder_path})
         return get_main_directory()
     
